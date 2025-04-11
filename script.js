1fafc21799c885d15cfc4bf7a8e4f5a0b4c7e802
MarcoCotas <marco.cotas06@gmail.com>
2025-04-11 20:54:40 +0100

    Create the  structure of the 3 files for the library

---
+++ script.js
@@ -0,0 +1,23 @@
+const title = document.getElementById("title")
+const author = document.getElementById("author")
+const pages = document.getElementById("pages")
+const submit = document.getElementById("submit")
+const myLibrary = [];
+
+function Book(name, author, pages ) {
+  // the constructor...
+  this.name = name
+  this.author = author
+  this.pages= pages
+}
+
+Book.prototype.
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+
+if (myLibrary.length === 0){
+  title.innerHTML = "hello"
+}
